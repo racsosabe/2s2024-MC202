532d602a56b3b1efa869c099c4971f70ffa452cd
racsosabe <racso_galvan@hotmail.com>
2024-09-04 19:34:39 -0300

    Added explanation of HW 1

--- 01-meteoros/1.c
+++ 01-meteoros/1.c
@@ -1,24 +1,16 @@
 #include<stdio.h>
 
-void swap(int *x, int *y) {
-	int c = *x;
-	*x = *y;
-	*y = c;
-}
-
 int main() {
 	int a, b, c, d;
 	int caso = 1;
 	while(scanf("%d %d %d %d", &a, &b, &c, &d) == 4) {
 		if(a == 0 && b == 0 && c == 0 && d == 0) break;
-		if(a > c) swap(&a, &c);
-		if(b > d) swap(&b, &d);
 		int n;
 		scanf("%d", &n);
 		int x, y, i, cnt = 0;
 		for(i = 0; i < n; i++) {
 			scanf("%d %d", &x, &y);
-			if(a <= x && x <= c && b <= y && y <= d) cnt += 1;
+			if(a <= x && x <= c && d <= y && y <= b) cnt += 1;
 		}
 		printf("Teste %d\n%d\n\n", caso++, cnt);
 	}
---
+++ 01-meteoros/README.md
@@ -0,0 +1,8 @@
+# Solução
+
+Para resolver o problema, basta contarmos quantas coordenadas $(x, y)$ satisfazem simultaneamente:
+
+- $a \leq x \leq c$
+- $d \leq y \leq b$
+
+Para ler múltiplos casos de entrada, usaremos `scanf` para a leitura e `while` para o loop. O loop terminará quando $a = b = c = d = 0$, conforme mencionado no enunciado.
